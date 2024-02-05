27c012eaaec1ffd3bad651e7c27555577843b9b7
VanessaSouldev <vanessasouldev@gmail.com>
2024-02-05 12:57:25 +1100

    completed-excercise

--- src/main/java/C_Oppervlakte.java
+++ src/main/java/C_Oppervlakte.java
@@ -2,21 +2,35 @@ public class C_Oppervlakte {
 
     public static void main(String[] args) {
         int antwoord = 0;
-        // Uncomment de regels en maak een method die voor het juiste antwoord zorgt.
 
-        //antwoord = oppervlakteRechthoek(2, 8);
+        antwoord = oppervlakteRechthoek(2, 8);
         System.out.println("De oppervlakte van een rechthoek met lengte 2 en breedte 8 is " + antwoord);
 
-        //antwoord = oppervlakteVierkant(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
+        antwoord = oppervlakteVierkant(5);
         System.out.println("De oppervlakte van een vierkant met zijden van 5 cm is " + antwoord);
 
-        //antwoord = oppervlakteDoos(2,8,5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
+        antwoord = oppervlakteDoos(2,8,5);
         System.out.println("De oppervlakte van een doos met lengte 2, breedte 8 en hoogte 5 is " + antwoord);
 
-        //antwoord = oppervlakteKubus(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteVierkant()` hergebruiken, of `oppervlakteDoos()` hergebruiken.
+        antwoord = oppervlakteKubus(5);
         System.out.println("De oppervlakte van een kubus met zijden van 5 cm is " + antwoord);
     }
+    public static int oppervlakteRechthoek(int x, int y) {
+        return x * y;
+    }
+    public static int oppervlakteVierkant(int x) {
+        return x * x;
+    }
 
-    // nieuwe methods komen hier
+    public static int oppervlakteDoos(int x, int y, int z) {
+        int bottomAndTop = x * y * 2;
+        int frontAndBack = x * z * 2;
+        int leftAndRight = y * z * 2;
+        return bottomAndTop + frontAndBack + leftAndRight;
+    }
+
+    public static int oppervlakteKubus(int x) {
+        return x * x * 6;
+    }
 
 }
